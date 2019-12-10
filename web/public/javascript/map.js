7a693c8c6e335a5c78cd7b767f8e10f885efff28
Federico Pfahler <federico.pfahler@usi.ch>
2019-12-10 11:26:35 +0100

    Add new position

--- web/public/javascript/map.js
+++ web/public/javascript/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.495, -0.09], 15);
+var map = L.map('map').setView([46.0037, 8.9511], 15);
 var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmLayer = new L.TileLayer(osmUrl, {
     maxZoom: 19,
@@ -6,7 +6,7 @@ var osmLayer = new L.TileLayer(osmUrl, {
 });
 map.addLayer(osmLayer);
 var markers = [];
-var marker1 = L.marker([51.497, -0.09],{title:"marker_1"}).addTo(map).bindPopup("Marker 1");
+var marker1 = L.marker([46.0037, 8.9511],{title:"marker_1"}).addTo(map).bindPopup("Marker 1");
 markers.push(marker1);
 
 marker1.on('mouseover',function(ev) {
@@ -14,7 +14,7 @@ marker1.on('mouseover',function(ev) {
 });
 
 
-var circle = L.circle([51.497, -0.09], 500, {
+var circle = L.circle([46.0037, 8.9511], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
