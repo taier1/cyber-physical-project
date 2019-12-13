7eb1f3ec0bfe026b46769e00c3842d580e57ba3b
Gabriele Di Rosa <dirosg@usi.ch>
2019-12-13 14:10:37 +0100

    Dynamic zoom

--- web/public/javascripts/map.js
+++ web/public/javascripts/map.js
@@ -1,29 +1,47 @@
 var markers = [];
 var map;
 
-var bikeIcon = L.icon({
-    iconUrl: '../images/bike.png',
-
-    iconSize:     [46, 45], // size of the icon
-    iconAnchor:   [23, 0], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
-
 let mapSetup = function () {
-    map = L.map('map').setView([46.0037, 8.9511], 15);
+    map = L.map('map', {
+        minZoom: 11
+    }).setView([46.0037, 8.9511], 15);
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmLayer = new L.TileLayer(osmUrl, {
         maxZoom: 19,
         attribution: 'Map data © OpenStreetMap contributors'
     });
     map.addLayer(osmLayer);
-}
+};
 
 let addMarker = function(lat, long, title, current, color) {
-    var marker1;
+    let marker1;
 
-    if (current)
-        marker1 = L.marker([lat, long], {icon: bikeIcon},{title: title}).addTo(map);
+    if (current) {
+        let bikeIcon = L.icon({
+            iconUrl: '../images/bike.png',
+
+            iconSize:     [map.getZoom()*0.5, map.getZoom()*0.5], // size of the icon
+            iconAnchor:   [23, 0], // point of the icon which will correspond to marker's location
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        marker1 = L.marker([lat, long], {icon: bikeIcon},{title: title});
+        marker1.addTo(map);
+
+        map.on('zoomend', function() {
+            let bikeIconZoom = L.icon({
+                iconUrl: '../images/bike.png',
+
+                iconSize:     [map.getZoom()*1.5, map.getZoom()*1.5], // size of the icon
+                iconAnchor:   [23, 0], // point of the icon which will correspond to marker's location
+                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            });
+            map.removeLayer(marker1);
+            marker1 = L.marker([lat, long], {icon: bikeIconZoom},{title: title});
+            marker1.addTo(map);
+            // marker1.addTo(map);
+        });
+    }
 
     if (!current) {
         var circle = L.circle([lat, long], 50, {
@@ -34,16 +52,16 @@ let addMarker = function(lat, long, title, current, color) {
 
         markers.push(marker1);
     }
-}
+};
 
 let _markerFunction = function(id){
     for (var i in markers){
         var markerID = markers[i].options.title;
         if (markerID == id){
             markers[i].openPopup();
-        };
+        }
     }
-}
+};
 
 $("a").click(function(){
     markerFunction($(this)[0].id);
